2fb37fbf2b4b90fe0346dff619ce42af332a80c6
Julio Gil <120219346+JulioGilCa@users.noreply.github.com>
2023-06-14 18:49:15 +0000

    Mejorado el css y preparado para presentar

--- src/js/component/Todolist.jsx
+++ src/js/component/Todolist.jsx
@@ -1,76 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/index.css";
 
-class TodoList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tasks: [],
-      taskCount: 0,
-    };
-  }
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [taskCount, setTaskCount] = useState(0);
 
-  handleKeyPress = (event) => {
+  const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       const task = event.target.value;
       if (task) {
-        this.setState((prevState) => ({
-          tasks: [...prevState.tasks, task],
-          taskCount: prevState.taskCount + 1,
-        }));
+        setTasks((prevTasks) => [...prevTasks, task]);
+        setTaskCount((prevTaskCount) => prevTaskCount + 1);
         event.target.value = "";
       }
     }
   };
 
-  handleDeleteTask = (index) => {
-    this.setState((prevState) => ({
-      tasks: prevState.tasks.filter((_, i) => i !== index),
-      taskCount: prevState.taskCount - 1,
-    }));
+  const handleDeleteTask = (index) => {
+    setTasks((prevTasks) =>
+      prevTasks.filter((_, i) => i !== index)
+    );
+    setTaskCount((prevTaskCount) => prevTaskCount - 1);
   };
 
-  render() {
-    const { tasks, taskCount } = this.state;
-
-    return (
-      // <div className="d-flex justify-content-center flex-column m-auto col-xl-4 col-8">
-      <div className="container m-auto col-xl-4 col-8">
-        <div className="header">
-          <h1>Todo List</h1>
-          <p>Tareas: {taskCount}</p>
-        </div>
-        <div className="container m-auto">
-          <input
-            type="text"
-            placeholder="Añade una tarea, pulsa Enter"
-            onKeyDown={this.handleKeyPress}
-          />
-          </div>
-          <div className="container task-box">
-          {tasks.length === 0 ? (
-            <p className="my-3">No hay tareas, añade tareas</p>
-          ) : (
-            <ul>
-              {tasks.map((task, index) => (
-                <div className="viewTask">
-                  <li key={index}>
-                    <div>{task}</div>
-                  </li>
-                  <button
-                    className="addTask"
-                    onClick={() => this.handleDeleteTask(index)}
-                  >
-                    Eliminar
-                  </button>
-                </div>
-              ))}
-            </ul>
-          )}
-        </div>
+  return (
+    <div className="container col-xl-4 col-8">
+      <div className="header">
+        <h1>Todo List</h1>
+        <p>Tareas: {taskCount}</p>
       </div>
-    );
-  }
-}
+      <div>
+        <input className="input-box w-100"
+          type="text"
+          placeholder="Añade una tarea, pulsa Enter"
+          onKeyDown={handleKeyPress}
+        />
+      </div>
+      <div className="task-box">
+        {tasks.length === 0 ? (
+          <p className="my-3">No hay tareas, añade tareas</p>
+        ) : (
+          <ul>
+            {tasks.map((task, index) => (
+              <div className="viewTask">
+                <li key={index}>
+                  <div>{task}</div>
+                </li>
+                <button
+                  className="addTask"
+                  onClick={() => handleDeleteTask(index)}
+                >
+                  Eliminar
+                </button>
+              </div>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default TodoList;
